6a21b7d6093e90175b3852fd550cda4459f5084d
Lộc Võ Đức Nguyễn <locvdnguyen@kms-technology.com>
2021-11-25 15:59:42 +0700

    add parameter to mock test

--- test_source.py
+++ test_source.py
@@ -6,7 +6,6 @@ from datetime import datetime
 
 
 class TestSource(unittest.TestCase):
-
     def test_convert_string_to_datetime(self):
         inputs = ["02/17/1999", "05/26/1999", "12/12/0001"]
         expected_results = [
@@ -30,34 +29,40 @@ class TestSource(unittest.TestCase):
             actual_age = source.calculate_age(inputs[index])
             self.assertEqual(expected_ages[index], actual_age)
 
-    @patch("source.pd.read_csv")
-    def test_read_csv(self, mock_pd_read_csv):
-        mock_pd_read_csv.return_value = pd.DataFrame({
-            "firstname": ["Loc", "Witch"],
-            "lastname": ["Nguyen", "Harry"],
-            "middlename": ["Duc", "Potter"],
-            "birthdate": ["02/17/1999", "01/12/2004"],
-            "country": ["VN", "England"]
-        })
-        expected_result = [
-            {
-                "firstname": "Loc",
-                "lastname": "Nguyen",
-                "middlename": "Duc",
-                "birthdate": "02/17/1999",
-                "country": "VN",
-                "fullname": "Nguyen Duc Loc",
-                "age": 22
-            },
-            {
-                "firstname": "Witch",
-                "lastname": "Harry",
-                "middlename": "Potter",
-                "birthdate": "01/12/2004",
-                "country": "England",
-                "fullname": "Harry Potter Witch",
-                "age": 17
-            }
-        ]
-        actual_result = source.read_csv("addresses.csv")
-        self.assertEqual(expected_result, actual_result)
+    def test_read_csv(self):
+        with patch("source.pd.read_csv") as mock_pd_read_csv:
+            mock_pd_read_csv.return_value = pd.DataFrame({
+                "firstname": ["Loc", "Witch"],
+                "lastname": ["Nguyen", "Harry"],
+                "middlename": ["Duc", "Potter"],
+                "birthdate": ["02/17/1999", "01/12/2004"],
+                "country": ["VN", "England"]
+            })
+            expected_result = [
+                {
+                    "firstname": "Loc",
+                    "lastname": "Nguyen",
+                    "middlename": "Duc",
+                    "birthdate": "02/17/1999",
+                    "country": "VN",
+                    "fullname": "Nguyen Duc Loc",
+                    "age": 22
+                },
+                {
+                    "firstname": "Witch",
+                    "lastname": "Harry",
+                    "middlename": "Potter",
+                    "birthdate": "01/12/2004",
+                    "country": "England",
+                    "fullname": "Harry Potter Witch",
+                    "age": 17
+                }
+            ]
+            file_name = "my-address.csv"
+            actual_result = source.read_csv(file_name)
+
+            mock_pd_read_csv.assert_called_with(file_name)
+            self.assertEqual(expected_result, actual_result)
+
+    def test_save_as_csv(self):
+        pass
