fe05742d0e499ec8794fd24431c877cac55b5948
Lộc Võ Đức Nguyễn <locvdnguyen@kms-technology.com>
2021-12-06 22:28:11 +0700

    update algorithm, add logging + handle exception

--- exercise01/main.py
+++ exercise01/main.py
@@ -6,18 +6,35 @@ Exercise 01 requirements:
 """
 
 import csv
+import logging.config
 from typing import List
+
+logging.config.fileConfig("log/logging.conf")
+log = logging.getLogger("exercise01Logger")
 
 
 def load_file(file_name: str) -> list:
     """
-    load file to a list of dictionaries. Each dictionary is equivalent a row in file.
+     Load file to a list of dictionaries.
+     Each dictionary is equivalent a row in file.
     """
 
-    with open(file=file_name, mode='r', encoding='utf-8') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        data = list(csv_reader)
-    return data
+    log.info("Loading file %s...", file_name)
+    try:
+        with open(file=file_name, mode='r', encoding='utf-8') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            data = list(csv_reader)
+        return data
+
+    except FileNotFoundError:
+        log.error("File name %s not found!", file_name)
+        raise
+    except IOError:
+        log.error("Error while reading the %s file...", file_name)
+        raise
+    except Exception as exception:
+        log.exception("Unexpected error occurs...\nMessage: %s", exception)
+        raise
 
 
 def filter_data(data_1: List[dict], data_2: List[dict]) -> List[dict]:
@@ -25,19 +42,52 @@ def filter_data(data_1: List[dict], data_2: List[dict]) -> List[dict]:
     Filter from data_1 and data_2,
     return a list of rows whose state is not null or site_number is not null
     """
-    accepted_license_numbers = set()
-    output = []
-    for line in data_1:
-        if line["state"]:
-            output.append(line)
-            accepted_license_numbers.add(line["license_number"])
 
-    for line in data_2:
-        if line["site_number"] \
-                and line["license_number"] not in accepted_license_numbers:
-            output.append(line)
+    log.info("Filtering data from data_1 and data_2 where "
+             "state or site number is not null...")
 
-    return output
+    try:
+        license_state_map = {}
+        for line in data_1:
+            license_number = line["license_number"]
+            state = line["state"]
+            if state:
+                license_state_map[license_number] = state
+
+        license_site_map = {}
+        for line in data_2:
+            license_number = line["license_number"]
+            site_number = line["site_number"]
+            if site_number:
+                license_site_map[license_number] = site_number
+
+        output = []
+        available_licenses_numbers = set()
+        for line in data_1:
+            license_number = line["license_number"]
+            state = line["state"]
+            if not state and license_number not in license_site_map.keys():
+                continue
+
+            if license_number in license_site_map:
+                line["site_number"] = license_site_map.get(license_number)
+            output.append(line)
+            available_licenses_numbers.add(license_number)
+
+        for line in data_2:
+            license_number = line["license_number"]
+            site_number = line["site_number"]
+            if not site_number and license_number not in license_state_map:
+                continue
+
+            if site_number and license_number not in available_licenses_numbers:
+                output.append(line)
+                available_licenses_numbers.add(license_number)
+
+        return output
+    except Exception as exception:
+        log.exception("Error while filtering data...\nMessage: %s", exception)
+        raise
 
 
 def write_to_file(file_name: str, data: List[dict]):
@@ -46,11 +96,21 @@ def write_to_file(file_name: str, data: List[dict]):
     :param file_name: a string - represents the location to write data
     :return:
     """
-    fieldnames = ["first_name", "last_name", "license_number", "state", "site_number"]
-    with open(file=file_name, mode="w", encoding="utf-8") as csv_file:
-        csv_writer = csv.DictWriter(f=csv_file, fieldnames=fieldnames)
-        csv_writer.writeheader()
-        csv_writer.writerows(data)
+
+    log.info("Writing data to %s...", file_name)
+    try:
+        fieldnames = ["first_name", "last_name", "license_number",
+                      "state", "site_number"]
+        with open(file=file_name, mode="w", encoding="utf-8") as csv_file:
+            csv_writer = csv.DictWriter(f=csv_file, fieldnames=fieldnames)
+            csv_writer.writeheader()
+            csv_writer.writerows(data)
+    except IOError:
+        log.error("Error while writing data to %s", file_name)
+        raise
+    except Exception as exception:
+        log.exception("Unexpected error occurred!\nMessage: %s", exception)
+        raise
 
 
 def main():
